1349398f1532ccd67b340cae2999f3033fcb6b3a
Hua Anh Minh <huaanhminh0412@gmail.com>
2020-12-14 22:38:33 +0700

    Add basic props for category model

---
+++ src/models/category.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    default: '',
+    trim: true,
+  },
+  parentCategory: {
+    type: String,
+    required: true,
+    default: '',
+    trim: true,
+  },
+  subCategories: [{
+    category: {
+      type: String,
+      required: true,
+      default: '',
+      trim: true,
+    },
+  }],
+}, {
+  timestamps: true,
+});
+
+categorySchema.methods.toJSON = function () {
+  const category = this;
+  const categoryObject = category.toObject();
+
+  delete categoryObject.__v;
+  delete categoryObject.createdAt;
+  delete categoryObject.updatedAt;
+
+  return categoryObject;
+};
+
+const Category = mongoose.model('Category', categorySchema);
+module.exports = Category;
