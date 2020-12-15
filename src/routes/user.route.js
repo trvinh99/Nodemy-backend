2da433e57f89e57a4cd28804c7695954a1a04578
Hua Anh Minh <huaanhminh0412@gmail.com>
2020-12-15 17:40:28 +0700

    Add request body schema for register

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,12 +1,22 @@
 const express = require('express');
 
+const User = require('../models/user.model');
+const requestBodyValidation = require('../middlewares/requestBodyValidation.middleware');
+
+const registerRequest = require('../requests/register.request.json');
+
 const userRoute = express.Router();
 
-userRoute.post('/users', async (req, res) => {
+userRoute.post('/users', requestBodyValidation(registerRequest), async (req, res) => {
   try {
-    
+    const user = new User(req.body);
+    await user.save();
+
+    res.status(201).send({
+      message: '',
+    });
   }
   catch (error) {
-
+    
   }
 });
