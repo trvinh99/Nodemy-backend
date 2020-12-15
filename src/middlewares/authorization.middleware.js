682418b357d6b2ae10cb57cd5728a02f633a9a51
Hua Anh Minh <huaanhminh0412@gmail.com>
2020-12-15 16:12:45 +0700

    Add authorization and authentication

---
+++ src/middlewares/authorization.middleware.js
@@ -0,0 +1,32 @@
+const RefreshToken = require('../models/refreshToken.model');
+
+const authorization = async (req, res, next) => {
+  try {
+    let token = req.header('Nodemy-Authorization');
+    if (!token || typeof token !== 'string') {
+      throw new Error();
+    }
+
+    token = token.replace('Bearer ', '');
+
+    const accessData = await RefreshToken.findOne({ token });
+    if (!accessData) {
+      throw new Error();
+    }
+
+    if ((new Date()).valueOf() > accessData.expiredAt) {
+      throw new Error();
+    }
+
+    req.userId = accessData.userId;
+
+    next();
+  }
+  catch {
+    res.status(401).send({
+      error: 'Please authorize.',
+    });
+  }
+};
+
+module.exports = authorization;
