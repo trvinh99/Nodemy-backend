40945e3b1dfa90cef1dd7471a530bf56ebba3b51
Hua Anh Minh <huaanhminh0412@gmail.com>
2020-12-16 15:38:36 +0700

    Add host to env

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.get('/', (_, res) => {
   });
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -105,6 +105,8 @@ userSchema.methods.toJSON = function () {
   delete userObject.updatedAt;
   delete userObject.__v;
 
+  userObject.avatar = `${process.env.HOST}/users/me/avatar`;
+
   return userObject;
 };
 
