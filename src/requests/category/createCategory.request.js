9274d0aa69ee1165426ac368770f5ff8ed371f2b
shint1001 <48145607+shint1001@users.noreply.github.com>
2020-12-19 10:57:10 +0700

    syntax fixed category.route

--- src/requests/category/createCategory.request.js
+++ src/requests/category/createCategory.request.js
@@ -2,40 +2,37 @@ const NodemyResponseError = require("../../utils/NodemyResponseError");
 
 const createCategoryRequest = ({ body }) => {
   if (typeof body !== "object") {
-    throw new NodemyResponseError(
-      400,
-      "Type of create category body is invalid!"
-    );
+    throw new NodemyResponseError(400, "Type of create category's body must be object!");
   }
-  const { name, parentCategory, description, subCategories, ...rest } = body;
+  const {
+    name,
+    parentCategory,
+    description,
+    subCategories,
+    ...rest
+  } = body;
 
   if (Object.keys(rest).length !== 0) {
-    throw new NodemyResponseError(400, "Create category body is invalid!");
+    throw new NodemyResponseError(400, "Create category's body has redundant field(s)!");
   }
 
   if (typeof name !== "string") {
-    throw new NodemyResponseError(400, "Type of category's name is invalid!");
+    throw new NodemyResponseError(400,"Type of category's name must be string!");
   }
 
   if (typeof parentCategory !== "string") {
-    throw new NodemyResponseError(400, "Type of parent category is invalid!");
+    throw new NodemyResponseError(400,"Type of parent's category must be string!");
   }
   if (parentCategory.length !== 24) {
-    throw new NodemyResponseError(400, "Format of parent category is invalid!");
+    throw new NodemyResponseError(400, "Parent category must contain 24 characters!");
   }
 
   if (typeof description !== "string") {
-    throw new NodemyResponseError(
-      400,
-      "Type of category description is invalid!"
-    );
+    throw new NodemyResponseError(400,"Type of category's description must be string!");
   }
 
   if (!Array.isArray(subCategories)) {
-    throw new NodemyResponseError(
-      400,
-      "Type of category's sub categories is invalid!"
-    );
+    throw new NodemyResponseError(400,"Type of category's sub categories must be array!");
   }
 };
 
