47fa1cf6a7b35e818863c54aff9c36aae9303f58
shint1001 <48145607+shint1001@users.noreply.github.com>
2020-12-21 23:18:53 +0700

    SectionRoute

---
+++ src/requests/sections/createSection.request.js
@@ -0,0 +1,41 @@
+const NodemyResponseError = require("../../utils/NodemyResponseError");
+
+const createSectionRequest = ({ body }) => {
+  if (typeof body !== "object") {
+    throw new NodemyResponseError(400, "Type of create section's body must be object!");
+  }
+  const {
+    courseId,
+    sectionName,
+    lectures,
+    ...rest
+  } = body;
+
+  if (Object.keys(rest).length !== 0) {
+    throw new NodemyResponseError(400, "Create section's body has redundant field(s)!");
+  }
+
+  if (typeof sectionName !== "string") {
+    throw new NodemyResponseError(400,"Type of section's name must be string!");
+  }
+
+  if (sectionName.length > 200) {
+      throw new NodemyResponseError(400,"Section's name must contain less than 200 characters!");
+  }
+
+  if (typeof lectures !== 'undefined') {
+    if (!Array.isArray(lectures)) {
+      throw new NodemyResponseError(400,"Type of section's lectures must be array!");
+    }
+  }
+
+  if (typeof courseId !== "string") {
+    throw new NodemyResponseError(400,"Type of section's course id must be string!");
+  }
+    
+  if (courseId.length !== 24) {
+      throw new NodemyResponseError(400, "Course id must contain 24 characters!");
+    }
+};
+
+module.exports = createSectionRequest;
---
+++ src/routes/section.route.js
@@ -0,0 +1,131 @@
+const express = require('express');
+
+const authentication = require('../middlewares/authentication.middleware');
+const rolesValidation = require('../middlewares/rolesValidation.middleware');
+const Course = require('../models/course.model');
+
+const Section = require('../models/courseSection.model');
+
+const requestValidation = require('../middlewares/requestValidation.middleware');
+const createSectionRequest = require('../requests/sections/createSection.request');
+const updateSectionRequest = require('../requests/sections/updateSection.request');
+
+const sectionError = require('../responses/section/section.response');
+
+const sectionRoute = express.Router();
+
+
+sectionRoute.get('/sections/teacher/:courseId', authentication, rolesValidation(['Teacher', 'Admin']) ,async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.courseId);
+        if (course.tutor !== req.user._id.toString()) {
+            res.status(404).send({ error: 'Found no course!'});
+        }
+        
+        const sections = await Section.find({ name: req.query.sectionName });
+
+        res.send({ sections }); 
+    }
+    catch (error) {
+        res.status(500).send({ error: "Internal Server Error" });
+    }
+})
+
+sectionRoute.get('/sections/:courseId', async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.courseId);
+        if (course.isPublic !== true) {
+            res.status(404).send({ error: 'Found no course!'});
+        }
+
+        const sections = await Section.find({ name: req.query.sectionName });
+
+        res.send({ sections });
+    }
+    catch (error) {
+        res.status(500).send({ error: "Internal Server Error" });
+    }
+})
+
+sectionRoute.post('/sections', authentication, rolesValidation(['Teacher']), requestValidation(createSectionRequest), async (req, res) => {
+    try {
+        const course = await Course.findById(req.body.courseId);
+        if (!course) {
+            res.status(404).send({ error: `Found no course with id ${req.body.courseId}!` });
+        }
+
+        const section = new Section(req.body);
+
+        await section.save();
+        res.status(201).send({ section })
+    } 
+    catch (error) {
+        res.status(400).send({
+            error: sectionError.createSectionError(error)
+        })
+    }
+})
+
+sectionRoute.delete('/sections/:id', authentication, rolesValidation(['Teacher', 'Admin']), async (req, res) => {
+    try {
+        const section = await Section.findById(req.params.id);
+        if (!section) {
+            res.status(404).send({ error: `Found no section!` });
+        }
+
+        await section.delete();
+
+        res.send({ section });
+    }
+    catch (error) {
+        res.status(400).send({
+           error: `Delete failed!`,
+        })
+    }
+})
+
+sectionRoute.patch('/sections/:id', authentication, rolesValidation(['Teacher', 'Admin']), requestValidation(updateSectionRequest), async (req, res) => {
+    try {
+        const section = await Section.findById(req.params.id);
+        if (!section) {
+            res.status(404).send({ error: `Found no section!` });
+        }
+
+        let hasChanged = false;
+
+        if (typeof req.body.courseId !== 'undefined' && section.courseId !== req.body.courseId) {
+
+            const course = await Course.findById(req.body.courseId);    
+            if (!course) {
+                res.status(404).send({ error: `Found no course!` });
+            }
+            
+            hasChanged = true;
+            section.courseId = req.body.courseId;
+        }
+
+        if (typeof req.body.lectures !== 'undefined' && section.lectures !== req.body.lectures) {
+            hasChanged = true;
+            section.lectures = req.body.lectures;
+        }
+
+        if (typeof req.body.sectionName !== 'undefined' && section.sectionName !== req.body.sectionName) {
+            hasChanged = true;
+            section.sectionName = req.body.sectionName;
+        }
+
+        if (hasChanged) {
+            await section.save();
+        }
+
+        res.send({ section })
+    }
+    catch (error) {
+        console.log(error.message)
+        res.status(400).send({
+           error: sectionError.updateSectionError(error),
+        })
+    }
+})
+
+module.exports = sectionRoute;
