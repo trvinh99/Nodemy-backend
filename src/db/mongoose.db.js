0f615fab813ee1af439e4b9cc82994f7c90fd45c
Hua Anh Minh <huaanhminh0412@gmail.com>
2020-12-27 14:29:51 +0700

    Fix url connection string db

--- src/db/mongoose.db.js
+++ src/db/mongoose.db.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
+let url = '';
+if (process.env.PHASE === 'DEVELOPMENT') {
+  url = `mongodb://127.0.0.1/nodemy-development`;
+}
+else {
+  const {
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB
+  } = process.env;
+  
+  url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+}
+
+mongoose.connect(url, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: true,
   useUnifiedTopology: true,
+  connectTimeoutMS: 10000,
 })
 .then(() => {
   console.log('Succeed to connect to database');
