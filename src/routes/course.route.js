a24e3e2da7080ad5f60aee18f5b786146aff563a
Hua Anh Minh <huaanhminh0412@gmail.com>
2021-01-08 15:21:52 +0700

    Add logger

--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -245,10 +245,11 @@ courseRoute.get('/courses/:id', bypassAuthentication, requestValidation(getCours
     course = await Course.increaseTotalViewed(course._id.toString());
 
     let isAdminOrOwner = false;
-    if (req.user.accountType === 'Admin') {
+    console.log(req.user.accountType);
+    if (req.user && req.user.accountType === 'Admin') {
       isAdminOrOwner = true;
     }
-    else if (req.user._id.toString() === course.tutor) {
+    else if (req.user && req.user._id.toString() === course.tutor) {
       isAdminOrOwner = true;
     }
 
